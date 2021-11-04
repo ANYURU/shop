ec0315d2cc4705684b59bc96b665eb820d99e1f5
anyuru <davidderrickanyuru@gmail.com>
2021-11-04 21:27:22 +0300

    Context partly implemented

---
+++ src/components/views/NotAuthorized.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotAuthorized() {
+    return (
+        <div>
+            Not allowed to access this resource
+        </div>
+    )
+}
+
+export default NotAuthorized
