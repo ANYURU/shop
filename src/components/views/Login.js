468575480ceeec660b44fbac1f16f3a880f8f20b
anyuru <davidderrickanyuru@gmail.com>
2021-11-06 23:31:48 +0300

    Refactored code in the login component.

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,19 +1,10 @@
-import {useState, useEffect} from 'react'
 import { useAuth } from '../contexts/Auth'
 import { useHistory } from 'react-router-dom'
 
 function Login() {
-    let [Login, setLogin] = useState(false)
     let { setCurrentUser} = useAuth()
 
     const history = useHistory()
-    useEffect(() => {
-        const loggedIn = Number(localStorage.getItem('loggedIn'))
-        if(loggedIn === 1){
-            setCurrentUser(loggedIn)
-            setLogin(loggedIn)
-        }
-    })
     return (
         <div>
             <button onClick={() => {
