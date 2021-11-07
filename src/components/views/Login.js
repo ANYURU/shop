1d6919a1080bd30587c9775cab7cf513a3729db3
anyuru <davidderrickanyuru@gmail.com>
2021-11-07 19:21:25 +0300

    Stay on the same page or component on page reload.

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,10 +1,22 @@
+import {useState, useEffect} from 'react'
 import { useAuth } from '../contexts/Auth'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Redirect } from 'react-router-dom'
 
 function Login() {
+    let [isLogin, setLogin] =useState(false)
     let { setCurrentUser} = useAuth()
-
     const history = useHistory()
+
+    useEffect(( ) => {
+        const loggedIn = Number(localStorage.getItem('loggedIn'))
+        if(loggedIn === 1) {
+            setCurrentUser(loggedIn)
+            setLogin(loggedIn)
+        }
+    })
+    
+    if(isLogin) return <Redirect to={{ pathname: '/dashboard'}}/>
+    
     return (
         <div>
             <button onClick={() => {
