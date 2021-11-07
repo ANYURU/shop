2b1013f33bacf7406d00905e4a08f31f0d2d6b17
anyuru <davidderrickanyuru@gmail.com>
2021-11-07 18:24:25 +0300

    Stay on tthe same page/component for authenticated users on page refresh.

--- src/components/routes/MyRouter.js
+++ src/components/routes/MyRouter.js
@@ -1,6 +1,6 @@
-import * as React from 'react'
 import PrivateRoute  from './PrivateRoute'
 import PublicRoute from './PublicRoute'
+import NotLoggedIn from '../views/NotLoggedIn'
 import Login from '../views/Login'
 import Home from '../views/Home'
 import Account from '../views/Account'
@@ -21,21 +21,21 @@ function MyRouter() {
     return (
         <Router>
             <Switch>
-                <PublicRoute path="/" exact>
+                <Route path="/" exact>
                     <Home />
-                </PublicRoute>
-                <PublicRoute path="/not-authorized">
-                    <NotAuthorized />
-                </PublicRoute>
-                <PublicRoute path="/login">
+                </Route>
+                <Route path="/not-authorized-in">
+                    <NotLoggedIn/>
+                </Route>
+                <Route path="/login">
                     <Login />
-                </PublicRoute>
-                <PublicRoute path="/cart">
+                </Route>
+                <Route path="/cart">
                     <Cart />
-                </PublicRoute>
-                <PublicRoute path="/checkout">
+                </Route>
+                <Route path="/checkout">
                     <Checkout />
-                </PublicRoute>
+                </Route>
                 <PrivateRoute path="/dashboard">
                     <Dashboard />
                 </PrivateRoute>
