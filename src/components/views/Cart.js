e2937ec9833f8a7aac1c2b1c895586896c6ddf79
anyuru <davidderrickanyuru@gmail.com>
2021-11-08 06:55:15 +0300

    Add to cart global state implementation.

--- src/components/views/Cart.js
+++ src/components/views/Cart.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useCart } from '../contexts/Cart'
 
 function Cart() {
+    const {itemsInCart } = useCart
+
     return (
         <div>
-            
+            {itemsInCart.map(itemInCart => <div key={itemInCart._id}>{itemInCart.name}</div>)}
         </div>
     )
 }
