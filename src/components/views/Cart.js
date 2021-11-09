0e496317d64e2481a464702b01abc96f937ef2e5
anyuru <davidderrickanyuru@gmail.com>
2021-11-09 07:47:30 +0300

    Corrected Cart total, subtotal, unitPrice, Qty logic.

--- src/components/views/Cart.js
+++ src/components/views/Cart.js
@@ -11,7 +11,8 @@ function Cart() {
     const { itemsInCart, setItemsInCart, total, setTotal } = useCart(0);
 
     useEffect(() => {
-        setTotal(itemsInCart.reduce(( currentItem, previousItem ) => currentItem.subtotal + previousItem.subtotal, 0))
+        let defaultTotal = 0
+        setTotal(itemsInCart.reduce(( currentItem, previousItem ) => defaultTotal += previousItem.subtotal, 0))
     },[])
 
     if(itemsInCart?.length >0 )
@@ -39,35 +40,45 @@ function Cart() {
                                         defaultValue={itemInCart.qty}
                                         placeholder={itemInCart.qty}
                                         onChange={event => {
-                                            const item = itemsInCart[index]
+                                            let { value: quantity } = event.target
 
-                                            // Question:
+                                            itemInCart.qty = quantity > 0 ? quantity : 1;
+                                            itemInCart.subtotal = itemInCart.qty * itemInCart.price;
+                                            setItemsInCart([...itemsInCart])
+
+                                            // [x] Question:
                                             // Using the const keyword on a Javascript object becomes a bit confusing 
                                             // — can the object's properties and methods be changed once defined ?
                                             // Yes, properties and methods be changed for a const object.
                                             // A const declaration does not mean that the value of the variable cannot be changed. It just means that the variable identifier cannot be reassigned.
 
-                                            item.qty = Number(event.target.value)
-                                            item['subtotal'] = Number(item.qty) * Number(item.price)
-                                            itemsInCart[index] = item
-                                            setItemsInCart([...itemsInCart])
-                                            const subTotals = itemsInCart.map(itemInCart => itemInCart.subtotal) 
-                                            const reducer = (currentValue, previousValue) => {
-                                                return currentValue + previousValue
-                                            }
-                                            setTotal(subTotals.reduce(reducer))
+                                            // item.qty = Number(event.target.value)
+                                            // item['subtotal'] = Number(item.qty) * Number(item.price)
+                                            // itemsInCart[index] = item
+                                            // setItemsInCart([...itemsInCart])
+                                            // const subTotals = itemsInCart.map(itemInCart => itemInCart.subtotal) 
+                                            // const reducer = (currentValue, previousValue) => {
+                                            //     return currentValue + previousValue
+                                            // }
+                                            // setTotal(subTotals.reduce(reducer))
+
+                                            let newTotal = 0
+                                            itemsInCart.forEach(itemInCart => newTotal += itemInCart.subtotal)
+                                            setTotal(newTotal);
+
+                                            
                                         }
                                     } />
                                    
-
                                 </td>
                                 <td>{formatter.format(itemInCart.price)}</td>
-                                <td>{formatter.format(itemInCart.subtotal || itemsInCart.price)}</td>
+                                <td>{formatter.format(itemInCart.subtotal)}</td>
                             </tr> 
                         )}
                     </tbody>
                     <tfoot>
                         <tr>
+                            <th></th>
                             <th></th>
                             <th>Total {formatter.format(total)}</th>
                         </tr>
